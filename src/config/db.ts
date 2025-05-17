25db672dc1f520c50f6213a3a730fa1c10bcdbbb
josebdo <74869200+josebdo@users.noreply.github.com>
2025-05-17 08:42:34 -0400

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,7 +4,9 @@ dotenv.config()
 
 const db = new Sequelize(process.env.DATABASE_URL!,{
     models: [__dirname + '/../models/**/*'],
-    logging: false
+    logging: false,
+    dialect: 'postgres',
+    dialectModule: require('pg')
 })
 
 export default db
